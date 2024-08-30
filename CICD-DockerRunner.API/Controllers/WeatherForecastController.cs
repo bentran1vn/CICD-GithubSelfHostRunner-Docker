ebbb13d29a78411b453a590140f892b61961047b
Tan Tran <thonguyen>
2024-08-31 01:35:30 +0700

    1231234

--- CICD-DockerRunner.API/Controllers/WeatherForecastController.cs
+++ CICD-DockerRunner.API/Controllers/WeatherForecastController.cs
@@ -53,4 +53,16 @@ public class WeatherForecastController : ControllerBase
         })
             .ToArray();
     }
+
+    [HttpGet(Name = "GetWeatherForecast3")]
+    public IEnumerable<WeatherForecast> Get3()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+            .ToArray();
+    }
 }
