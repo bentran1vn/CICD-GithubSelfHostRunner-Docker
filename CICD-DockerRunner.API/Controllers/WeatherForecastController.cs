dc6b4f1c2fb14fac849df8152fc00e30290dd1d3
bentran1vn <tan182205@gmail.com>
2024-09-09 09:33:45 +0700

    fix

--- CICD-DockerRunner.API/Controllers/WeatherForecastController.cs
+++ CICD-DockerRunner.API/Controllers/WeatherForecastController.cs
@@ -69,7 +69,7 @@ public class WeatherForecastController : ControllerBase
     [HttpGet(Name = "GetWeatherForecast4")]
     public IEnumerable<WeatherForecast> Get4()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        return Enumerable.Range(1, 4).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
